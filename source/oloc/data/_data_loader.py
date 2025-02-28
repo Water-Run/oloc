7a80d3aee2535cf2c455da349c2eee3804ad1034
ZhangRun <127740919+Water-Run@users.noreply.github.com>
2025-02-28 19:08:58 +0800

    writing documents

--- source/oloc/data/_data_loader.py
+++ source/oloc/data/_data_loader.py
@@ -2,28 +2,28 @@ r"""
 :author: WaterRun
 :date: 2025-02-28
 :file: _data_loader.py
-:description: 脚本程序,生成oloc运行中所需的各个表数据
+:description: Script program to generate various table data required for oloc runtime
 """
 
 import simpsave as ss
 
 r"""
-修改后在data路径下直接运行该脚本
+After modification, run this script directly in the "data" path
 """
 
-# 符号映射表
+# Symbol Mapping Table
 
 symbol_mapping_table: dict = {
 
 }
 
-# 函数转换表
+# Function Conversion Table
 
 function_conversion_table: dict = {
 
 }
 
-# 写入数据
+# Write Data
 
 ss.write('symbol_mapping_table', symbol_mapping_table, 'olocdata.ini')
 ss.write('function_conversion_table', function_conversion_table, 'olocdata.ini')
