caab85ed14a60278529f7e33fb137f9415a42e0e
ZhangRun <127740919+Water-Run@users.noreply.github.com>
2025-04-06 14:40:56 +0800

    writing oloc_release

--- source/oloc_release/__init__.py
+++ source/oloc_release/__init__.py
@@ -1,6 +1,12 @@
 r"""
 :author: WaterRun
-:date: 2025-04-03
+:date: 2025-04-06
 :file: __init__.py
 :description: Init oloc release
 """
+from oloc_cli import cli
+from oloc_gui import gui
+
+__all__ = [cli, gui]
+__version__ = "0.1.0"
+__author__ = "WaterRun"
--- source/oloc_release/oloc_gui.py
+++ source/oloc_release/oloc_gui.py
@@ -1,8 +1,18 @@
 r"""
 :author: WaterRun
-:date: 2025-04-03
+:date: 2025-04-06
 :file: oloc_gui.py
 :description: Oloc GUI
 """
 
 import tkinter as tk
+
+
+def gui():
+    r"""
+
+    :return:
+    """
+
+if __name__ == '__main__':
+    gui()
